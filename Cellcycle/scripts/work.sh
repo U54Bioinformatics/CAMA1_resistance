f032aac9c88e9413e428457d188193580ac96c82
Ben Decato <bdecato@gmail.com>
2021-08-11 08:52:54 -0700

    Removing copies

--- Cellcycle/scripts/work.sh
+++
@@ -1,6 +0,0 @@
-
-echo "cell cycle"
-export R_LIBS=/home/jichen/software/BETSY/install/envs/scRNA/lib/R/library
-cat VG_CAMA1_D11_ALL_10x_cell_number_vs_cell_cycle.R | /home/jichen/software/BETSY/install/envs/scRNA/bin/R --slave > VG_CAMA1_D11_ALL_10x_cell_number_vs_cell_cycle.chisq_test.txt
-
-
